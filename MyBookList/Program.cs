0597baa3eba0e7ebb7076fb799ff7c684dcb9672
denonia <denoniaa@gmail.com>
2024-04-19 15:33:13 +0300

    Scaffold default Login and Register views

--- MyBookList/Program.cs
+++ MyBookList/Program.cs
@@ -18,7 +18,12 @@ public class Program
             options.UseSqlServer(connectionString));
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+                options.SignIn.RequireConfirmedAccount = false;
+            })
             .AddEntityFrameworkStores<ApplicationDbContext>();
         builder.Services.AddRazorPages();
 
